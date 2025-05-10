7e275d72976a62f58d7862626ee61bea6741d5dc
Oageng Maphutha <oagengmaphutha00@gmail.com>
2025-05-10 10:11:08 +0200

    Create calculator.py
    Create a calculator.py program and define these five functions:
    
    add(a, b) that adds two numbers a and b.
    subtract(a, b) that subtracts two numbers a and b
    multiply(a, b) that multiplies two numbers a and b.
    divide(a, b) that divides two numbers a and b.
    exp(a, b) that takes a to the exponent (or power) of b.

---
+++ 6. Functions/calculator.py
@@ -0,0 +1,21 @@
+def add(a, b):
+  answer = a + b
+  return answer
+def subtract(a, b):
+  answer = a - b
+  return answer
+def multiply(a, b):
+  answer = a* b
+  return answer
+def divide(a, b):
+  answer = a/ b
+  return answer
+def exp(a, b):
+  answer = a** b
+  return answer     
+print(subtract(50, 40))    
+print(add(50, 40))  
+print(exp(5, 2)) 
+print(divide(50, 50))         
+
+
