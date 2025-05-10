58c7ce1a65408ac28bbc074f8b4df079fe30b174
Oageng Maphutha <oagengmaphutha00@gmail.com>
2025-05-10 09:47:40 +0200

    Create enter_pin.py

---
+++ 4. Loops/enter_pin.py
@@ -0,0 +1,7 @@
+print("====Bank of  Codedex====")
+pin = int(input("Enter Pin: "))
+while pin != 1234:
+  pin = int(input("Incorrect Pin.Enter your pin again: "))
+if pin == 1234:
+  print("Pin Accepted")
+
