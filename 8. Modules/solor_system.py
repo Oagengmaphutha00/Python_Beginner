8f9d8e09674be1829abb0fec268b1fc78af48d42
Oageng Maphutha <oagengmaphutha00@gmail.com>
2025-05-11 21:30:03 +0200

    Create solor_system.py

---
+++ 8. Modules/solor_system.py
@@ -0,0 +1,24 @@
+from math import pi
+from random import choice as ch
+planets = [
+  'Mercury',
+  'Venus',
+  'Earth',
+  'Mars',
+  'Saturn'
+]
+random_planet = ch(planets)
+if random_planet == 'Mercury':
+  radius = 2440
+elif random_planet ==  'Venus':
+  radius = 6052
+elif random_planet == 'Earth':
+  radius = 6371
+elif random_planet == 'Mars':
+  radius =3390
+elif random_planet == 'Saturn':
+  radius = 58232
+else:
+  print("Oops! An error occurred")
+area = 4 * pi * (radius ** 2)
+print(area)
